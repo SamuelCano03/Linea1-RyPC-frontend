2d34c01148b3cebf209d89a42310d1f92dcac06c
EduardoPuglisevich <eduardopugli2508@gmail.com>
2024-06-24 23:23:04 -0500

    add icon

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment={
     producction: false,
-    /* apiUrl: 'http://localhost:8080/api/v1' */
-    apiUrl: 'https://linea1-back3.azurewebsites.net/api/v1'
+    apiUrl: 'https://linea1-back3.azurewebsites.net/api/v1' 
+    /* apiUrl: 'http://localhost:8080/api/v1'  */
 }
